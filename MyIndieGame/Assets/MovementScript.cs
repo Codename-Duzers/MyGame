03202a29db9245f40dd9a554b18d8a91288c56c6
Codename-Duzers <81640890+Codename-Duzers@users.noreply.github.com>
2021-07-30 11:29:15 +0100

    Added Basic Terrestrial Movement
    I have added the code for running forward, and walking forwards, sideways, and backwards. I have continued with my idea of abstaining from running backwards and to both sides and have used the code from IHeartGamedev's youtube channel.

---
+++ MyIndieGame/Assets/MovementScript.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    Animator animator;
+    float velocityZ = 0.0f;
+    float velocityX = 0.0f;
+    public float acceleration = 0.2f;
+    public float deceleration = 2.0f;
+    public float maximumwalkVelocity = 0.5f;
+    public float maximumrunVelocity = 2.0f;
+
+    int VelocityZHash;
+    int VelocityXHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        //Change to Increase Performance. I'm using the string to Hash function, so I can change the variable to become an integer and we can replace
+        // our string parameters, the "Velocity z" and "Velocity x", to the stored version of a simpler data type, increasing performance.
+        VelocityZHash = Animator.StringToHash("VelocityZ");
+        VelocityXHash = Animator.StringToHash("VelocityX");
+    }
+
+    // Update is called once per frame
+
+    //handles acceleration and deceleration
+    void ChangeVelocity(bool forwardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity, bool backPressed)
+    {
+        if (forwardPressed && velocityZ < currentMaxVelocity)
+        {
+            velocityZ += Time.deltaTime * acceleration;
+        }
+
+        if (leftPressed && velocityX > -currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * acceleration;
+        }
+
+        if (rightPressed && velocityX < currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * acceleration;
+        }
+
+        if (backPressed && velocityZ < currentMaxVelocity)
+        {
+            velocityZ -= Time.deltaTime * acceleration;
+        }
+
+        if (!forwardPressed && velocityZ > 0.0f)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+        }
+
+        if (!leftPressed && velocityX < 0.0f)
+        {
+            velocityX += Time.deltaTime * deceleration;
+        }
+
+        if (!rightPressed && velocityX > 0.0f)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+        }
+
+        if (!backPressed && velocityZ < 0.0f)
+        {
+            velocityZ += Time.deltaTime * acceleration;
+        }
+    }
+
+    //handles reset and locking of velocity
+    void lockOrResetVelocity(bool forwardPressed, bool leftPressed, bool rightPressed, bool runPressed, float currentMaxVelocity, bool backPressed)
+    {
+        if (!leftPressed && !rightPressed && velocityX != 0.0f && (velocityX > -0.05f && velocityX < 0.05f))
+        {
+            velocityX = 0.0f;
+        }
+
+        if (!forwardPressed && !backPressed && velocityZ != 0.0f && (velocityZ > -0.05f && velocityZ < 0.05f))
+        {
+            velocityZ = 0.0f;
+        }
+
+        if (forwardPressed && runPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ = currentMaxVelocity;
+        }
+        else if (forwardPressed && velocityZ > currentMaxVelocity)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+
+            if (velocityZ > currentMaxVelocity && velocityZ < (currentMaxVelocity + 0.05))
+            {
+                velocityZ = currentMaxVelocity;
+            }
+        }
+        else if (forwardPressed && velocityZ < currentMaxVelocity && velocityZ > (currentMaxVelocity - 0.05f))
+        {
+            velocityZ = currentMaxVelocity;
+        }
+
+
+        if (rightPressed && velocityX > currentMaxVelocity)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+
+            if (velocityX > currentMaxVelocity && velocityX < (currentMaxVelocity + 0.05))
+            {
+                velocityX = currentMaxVelocity;
+            }
+        }
+        else if (rightPressed && velocityX < currentMaxVelocity && velocityX > (currentMaxVelocity - 0.05f))
+        {
+            velocityX = currentMaxVelocity;
+        }
+
+
+        if (leftPressed && velocityX < -currentMaxVelocity)
+        {
+            velocityX += Time.deltaTime * deceleration;
+
+            if (velocityX < -currentMaxVelocity && velocityX > (-currentMaxVelocity - 0.05f))
+            {
+                velocityX = -currentMaxVelocity;
+            }
+        }
+        else if (leftPressed && velocityX > -currentMaxVelocity && velocityX < (-currentMaxVelocity + 0.05f))
+        {
+            velocityX = -currentMaxVelocity;
+        }
+
+        if (backPressed && velocityZ < -currentMaxVelocity)
+        {
+            velocityZ += Time.deltaTime * deceleration;
+
+            if (velocityZ < -currentMaxVelocity && velocityZ > (-currentMaxVelocity - 0.05f))
+            {
+                velocityZ = -currentMaxVelocity;
+            }
+        }
+        else if (backPressed && velocityZ > -currentMaxVelocity && velocityZ < (-currentMaxVelocity + 0.05f))
+        {
+            velocityZ = -currentMaxVelocity;
+        }
+    }
+    void Update()
+    {
+        bool forwardPressed = Input.GetKey(KeyCode.W);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        bool backPressed = Input.GetKey("s");
+
+        //(Ternary operator) A ternary operator will set the first option after the question mark true if the first option is true otherwise,
+        //it will set the variable equal to the second option after the question mark.
+
+        float currentMaxVelocity = runPressed ? maximumrunVelocity : maximumwalkVelocity;
+
+        ChangeVelocity(forwardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity, backPressed);
+        lockOrResetVelocity(forwardPressed, leftPressed, rightPressed, runPressed, currentMaxVelocity, backPressed);
+
+        animator.SetFloat(VelocityZHash, velocityZ);
+        animator.SetFloat(VelocityXHash, velocityX);
+    }
+}
+
+//Credits to IheartGamedev's youtube channel for this movement script, go check him out, he does great tutorials about Unity and various other topics.
